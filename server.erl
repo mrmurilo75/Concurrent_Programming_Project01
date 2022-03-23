86eb759cd7064f5cdd9c3a12ae16b3d015514df9
mrmurilo75 <mrmurilo75@gmail.com>
2022-03-23 14:18:40 +0000

    Begin structure of server

--- server.erl
+++ server.erl
@@ -1,5 +1,19 @@
 -module(server).
--export([]).
+-export([calculate/0]).
 
-
+calculate() -> 
+	receive
+		{From, sum, Pol1, Pol2} -> 
+			From ! {error, not_implemented},
+			calculate();
+		{From, subtract, Pol1, Pol2} -> 
+			From ! {error, not_implemented},
+			calculate();
+		{From, multiply, Pol1, Pol2} -> 
+			From ! {error, not_implemented},
+			calculate();
+		{From, _, _, _} -> 
+			From ! {error, not_implemented},
+			calculate()
+	end.
 
