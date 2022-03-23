d61f7d59bd402bee1c943e2f496a7340e02cf009
mrmurilo75 <mrmurilo75@gmail.com>
2022-03-23 20:34:48 +0000

    continue implementing server

--- server.erl
+++ server.erl
@@ -1,10 +1,19 @@
 -module(server).
 -export([server/0]).
 
+% why does this not work ? : 
+% .. spawn(server, sum, [{From, Caller, Pol1, Pol2}]) ..
+% sum({From, Caller, Pol1, Pol2}) -> ..
+
+sum() -> 
+	receive 
+		{From, Caller, Pol1, Pol2} -> From ! {calculation_result, Caller, not_implemented}
+	end.
+
 server() -> 
 	receive
 		{From, sum, {Pol1, Pol2} } -> 
-			From ! {error, not_implemented},
+			spawn(server, sum, []) ! {self(), From, Pol1, Pol2},
 			server();
 		{From, subtract, {Pol1, Pol2} } -> 
 			From ! {error, not_implemented},
