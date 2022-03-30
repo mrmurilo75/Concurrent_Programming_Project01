3c3384f96873c30f4f8ab9fce49588ba022c40a0
mrmurilo75 <mrmurilo75@gmail.com>
2022-03-30 14:15:54 +0100

    add annotations for Report and for bugfix

--- server.erl
+++ server.erl
@@ -1,9 +1,6 @@
 -module(server).
 -export([start/0, server/0, send_request/2]).
 
-%% why does this not work ? :
-%% .. spawn(server, sum, [{ From, Caller, Pol1, Pol2 }]) ..
-%% sum({ From, Caller, Pol1, Pol2 }) -> ..
 
 %% Polynomial format: List of pairs { Coef, Factors }, where
 %% "Factors" is a list of variable names with their respective power.
@@ -27,7 +24,7 @@ server() ->
         { From, { subtract, { Poly1, Poly2 } } } ->
             From ! { success, diff(Poly1, Poly2) };
         { From, { multiply, { Poly1, Poly2 } } } ->
-            From ! { error, mul(Poly1, Poly2) };
+            From ! { success, mul(Poly1, Poly2) };
         { From, _ } ->
             From ! { error, bad_request };
         _ -> ok %% idk, ignore I guess
@@ -37,30 +34,43 @@ send_request(Server, Request) ->
     Server ! { self(), Request },
     receive Response -> Response end.
 
-find_and_remove(Elem, [Head | List], IsEqual) ->
-    case IsEqual(Elem, Head) of
-        true -> { Head, List };
-        _ -> case find_and_remove(Elem, List, IsEqual) of
-                 { Something, Rest } -> { Something, [Head | Rest] }
-             end
-    end;
+% put it in a single polynomial and reduce (add terms with equal factors)
+sum(Left, Right) ->
+    combine_terms(lists:append(Left, Right)).
 
-find_and_remove(_, [], _) ->
-    { false, [] }.
+% same as sum, but flip coefs
+diff(Left, Right) ->
+    combine_terms(
+      lists:append(
+        Left,
+        lists:map(fun ({ Coef, Something }) ->
+                          { -Coef, Something } end, Right)
+       )).
 
-is_subset([Elem | Left], Right) ->
-    lists:member(Elem, Right) and is_subset(Left, Right);
+mul(Left, Right) ->
+    combine_terms(distribute(Left, Right)).
 
-is_subset([], _) -> true.
+% TODO (we can implement this to accept unordered input and do faster sum)
+% implementing sort for factor list (comparing then will be trivial)
+% sort the polynomial by factor
+% reduce equal factors
 
-have_same_factors(Left, Right) ->
-    is_subset(Left, Right) and is_subset(Right, Left).
+% TODO multiplication
+% highly paralelable
+% after sorting as above
+% spawn new node for each term of Pol1:
+% 	multiply coefs
+% 	gets this factor, add it to the list, then sort/reduce factors
+% 	repeat for each term of Pol2
+% add all results
 
+% add coef of terms with equal Factors ( ex.: [{x,2}, {y,3}] )
 combine_terms([Head = { Coef1, Factors } | Poly]) ->
     case find_and_remove(
            Head,
            Poly,
-           fun ({ _, X }, { _, Y }) -> have_same_factors(X, Y) end) of
+           fun ({ _, X }, { _, Y }) -> have_same_factors(X, Y) end)
+    of
         { false, _ } ->
             [Head | combine_terms(Poly)];
         { { Coef2, _ }, Rest } ->
@@ -68,6 +78,32 @@ combine_terms([Head = { Coef1, Factors } | Poly]) ->
     end;
 
 combine_terms(_) -> [].
+
+
+find_and_remove(Elem, [Head | List], IsEqual) ->
+    case IsEqual(Elem, Head) of
+        true -> { Head, List };
+        _ -> case find_and_remove(Elem, List, IsEqual) of % cant we just : -> find_and_remove() end 
+                 { Something, Rest } -> { Something, [Head | Rest] }
+             end
+    end;
+
+find_and_remove(_, [], _) ->
+    { false, [] }.
+
+
+
+is_subset([Elem | Left], Right) ->
+    lists:member(Elem, Right) and is_subset(Left, Right);
+
+is_subset([], _) -> true.
+
+
+
+have_same_factors(Left, Right) ->
+    is_subset(Left, Right) and is_subset(Right, Left).
+
+
 
 combine_factors([Head = { Var, ThisPower } | Factors]) ->
     case find_and_remove(
@@ -87,20 +123,9 @@ distribute([{ Coef1, Factors1 } | Left], Right) ->
       fun ({ Coef2, Factors2 }) ->
               { Coef1 * Coef2, combine_factors(
                                  lists:append(Factors1, Factors2)
-                                ) } end, Right) ++ mul(Left, Right);
+                                ) } end, Right) ++ mul(Left, Right); % I think this still needs reducing after ( we need could have x^2 twice in factor, for example, and have the same factor with coefs twice too, ex x^2 * x^3 * y^2 * y^3 + .. + x^1 * x^4 * y^1 * y^4 ) 
+									% TODO solve this just sort and reduce
 
 distribute([], _) -> [].
 
-sum(Left, Right) ->
-    combine_terms(lists:append(Left, Right)).
 
-diff(Left, Right) ->
-    combine_terms(
-      lists:append(
-        Left,
-        lists:map(fun ({ Coef, Something }) ->
-                          { -Coef, Something } end, Right)
-       )).
-
-mul(Left, Right) ->
-    combine_terms(distribute(Left, Right)).
