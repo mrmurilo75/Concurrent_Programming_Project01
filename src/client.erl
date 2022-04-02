c4843b0a22fbda560e7d4043420f54a9a4dd63e9
Murilo Rosa <mrmurilo75@gmail.com>
2022-04-02 20:17:24 +0100

    use gen_tcp

--- src/client.erl
+++ src/client.erl
@@ -1,6 +1,30 @@
 -module(client).
--export([]).
+-export([send_request/1, send_request/2]).
 
 
+%% Polynomial format: List of pairs { Coef, Factors }, where
+%% "Factors" is a list of variable names with their respective power.
+%% For example, [{ 2, [{ x, 2 }, { y, 1 }] }] represents
+%% 2 * x^2 * y^1.
 
+%% Example of how to execute:
+%% erl
+%% c(client).
+%% client:send_request(Host, { sum, { [{ 2, [{ x, 2 }] }], [{ 1, [{ x, 2 }] }] } }).
+
+send_request(Req) ->
+	send_request("localhost", Req).
+
+send_request(Host, Req) ->
+	{ok, Socket} =
+		gen_tcp:connect(Host , 2345,
+				[binary, {packet, 4}]),
+	ok = gen_tcp:send(Socket, term_to_binary(Req)),
+	receive
+		{tcp,Socket,Bin} ->
+			%% io:format("Client received binary = ~p~n" ,[Bin]),
+			Val = binary_to_term(Bin),
+			io:format("Client result = ~p~n" ,[Val]),
+			gen_tcp:close(Socket)
+	end.
 
--- src/server.erl
+++ src/server.erl
@@ -1,23 +1,12 @@
 -module(server).
--export([start/0, server/0, send_request/2]).
-
-
-%% Polynomial format: List of pairs { Coef, Factors }, where
-%% "Factors" is a list of variable names with their respective power.
-%% For example, [{ 2, [{ x, 2 }, { y, 1 }] }] represents
-%% 2 * x^2 * y^1.
+-export([start/0]).
 
 %% Example of how to execute:
 %% erl
 %% c(server).
 %% Pid = server:start().
-%% server:send_request(Pid, { sum, { [{ 2, [{ x, 2 }] }], [{ 1, [{ x, 2 }] }] } }).
 
-start() -> spawn(?MODULE, server, []).
-
-%% Should reduce factors before passing polynomials to sum and diff,
-%% 'cause otherwise some factors may not be reduced.
-server() ->
+calculate() ->
     receive
         { From, { Operation, { Poly1, Poly2 } } } ->
             NormPoly1 = poly:normalize(Poly1),
@@ -33,8 +22,34 @@ server() ->
         { From, _ } ->
             From ! { error, bad_request };
         _ -> ok %% idk, ignore I guess
-    end, server().
+    end, calculate().
 
 send_request(Server, Request) ->
     Server ! { self(), Request },
     receive Response -> Response end.
+
+start() ->
+	{ok, Listen} = gen_tcp:listen(2345, [binary, {packet, 4},
+						{reuseaddr, true},
+						{active, true}]),
+	{ok, Socket} = gen_tcp:accept(Listen),
+	gen_tcp:close(Listen),
+	loop(Socket).
+
+loop(Socket) ->
+	receive
+		{tcp, Socket, Bin} ->
+			%% io:format("Server received binary = ~p~n" ,[Bin]),
+			Str = binary_to_term(Bin),
+			{sum, Req} = Str,
+			io:format("Server (unpacked) ~p~n" ,[is_tuple(Req)]),
+			Pid = spawn(?MODULE, calculate, []),
+			io:format("Pid = ~p~n" ,[Pid]),
+			Reply = send_request(Pid, Str),
+			%% io:format("Server replying = ~p~n" ,[Reply]),
+			gen_tcp:send(Socket, term_to_binary(Reply)),
+			loop(Socket);
+		{tcp_closed, Socket} ->
+			io:format("Server socket closed~n" )
+	end.
+
